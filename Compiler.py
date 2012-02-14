5ece513c8390272645f108ac15545b13b7d0277f
Kirby Iwaki Tsukino <kirby@linux-ilux.site>
2012-02-13 23:24:53 -0600

    Agregue Lexic,py y Compiler.py
    Ademas de test.ari con el que lo pueden probar. Descubri que no puedo
    poner acentos aqui. Me volvere loco :D

---
+++ Compiler.py
@@ -0,0 +1,20 @@
+﻿import sys
+import re
+import Lexic
+from Lexic import *
+
+def main():
+  if len(sys.argv)<=1:
+      print 'Usage: python Compiler.py <filename>'
+      return
+  
+  arg = sys.argv[1]
+  f = open(arg,'r')
+  fr = f.read()
+  Lex = Lexic()
+  for item in re.findall("\s*(\'.*\'|\d+\.\d+|\d+|\w+|.)", fr):
+    print Lex.getTypeName(item)
+  return
+
+if __name__ == '__main__':
+  main()
---
+++ Lexic.py
@@ -0,0 +1,51 @@
+﻿import re
+
+class Lexic(object):    
+  
+  def getType(self,item):
+    keywords = [
+      'if','else','return','main','while',
+      'for','do','int','float','char','string','print']
+    logicals = ['&&','||','!']
+    relationals = ['>','<','==','!=','>=','<=']
+    arithmetics = ['+','-','*','/','+=']
+    groupings = ['[',']',';','(',')','{','}']
+    #keyword
+    if item in keywords:
+      return 0
+    #int
+    elif re.search(r'^\d+$',item):
+      return 1
+    #float
+    elif re.search(r'^\d+\.\d+$',item):
+      return 2
+    #identifier
+    elif re.search(r'^[\w][\w|\d]*$',item):
+      return 3
+    #string
+    elif re.search(r'^\'.*\'$',item):
+      return 4
+    #logicalOp
+    elif item in logicals:
+      return 5
+    #relationalOp
+    elif item in relationals:
+      return 6
+    #arithmeticOp
+    elif item in arithmetics:
+      return 7
+    #assignmentOp  
+    elif item == '=':
+      return 8
+    #groupingOp
+    elif item in groupings:
+      return 9
+    #error  
+    else:
+      return -1
+      
+  def getTypeName(self,item):
+    typeNames = {0:'keyword',1:'int',2:'float',3:'identifier',4:'string',
+    5:'logical operator',6:'relational operator',7:'arithmetic operator',
+    8:'assignment operator',9:'grouping operator',-1:'error'}
+    return item + '\t<-\t' + typeNames[self.getType(item)]
